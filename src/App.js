633dec3ba4dc7da70d1f46ea10c62866c8c25f0e
Estela Parrado <estela.parrado@gmail.com>
2018-03-11 09:20:47 -0400

    fetch working and styles ok, working on filter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
-import PokemonList from './components/PokemonList';
-
+import PokemonItem from './components/PokemonItem';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonStore : [],
+      filter : ''
+    };
+
+    this.PokemonFiltered = this.PokemonFiltered.bind(this)
+
+  }
+
+  PokemonFiltered(event){
+   this.setState({
+     filter: event.target.value.toLowerCase()
+   });
+ }
+
+  componentDidMount () {
+    for (let id = 1; id <= 25; id++)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let pokemonJson = this.state.pokemonStore;
+        pokemonJson.push(data)
+        this.setState({
+        pokemonStore : pokemonJson
+      });
+    });
+  }
+
+  printPokemons () {
+    let pokemon = this.state.pokemonStore.filter(pokemons => pokemons.name.toLowerCase().includes(this.state.filter));
+
+    return(
+      <ul className="pokemon-list">
+        {pokemon.sort((a,b) => a.id - b.id).map((pokemons, index) =>
+          <li key={index} >
+            <PokemonItem
+              key={index}
+              id={pokemons.id}
+              name={pokemons.name}
+              types= {pokemons.types.map((t) => t.type.name)}
+              />
+          </li>
+        )}
+      </ul>
+    );}
+
   render() {
     return (
     <div className="App">
       <div className="introduction">
         <img className="pokeball-animation" src="images/pokeball.png" alt="pokeball"/>
-        <input className="input-style" type="text" name="buscar" placeholder="Introduce un nombre para buscar"/>
+        <input className="input-style" type="text" name="buscar" placeholder="Introduce un nombre para buscar" onChange={this.pokemonFiltered}/>
       </div>
-      <PokemonList />
+      {this.printPokemons()}
     </div>
   );
   }
--- src/components/PokemonItem.js
+++ src/components/PokemonItem.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 class PokemonItem extends Component {
   render(){
-    const {pokemon,id} = this.props;
     return(
-      <li className="pokemon-card">
-          <img className="pokemon-photo" src={`images/${id}.png`} alt={`images/${id}.png`} />
-          <h2 className="pokemon-name">{pokemon.name}</h2>
-      </li>
+      <div className="pokemon-card">
+          <img className="pokemon-photo" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`} alt= {this.props.name} />
+          <h2 className="pokemon-name">{this.props.name.toUpperCase()}</h2>
+          <h3 className="pokemon-id"> {this.props.id.toLocaleString('en-US', {minimumIntegerDigits: 2})}</h3>
+          <ul className="pokemon-sublist">
+            {this.props.types.map((type, index) =>
+              <li key={index} className={`${type.toLowerCase()}`}>
+              {type}
+              </li>)}
+          </ul>
+      </div>
 
     )}
 }
